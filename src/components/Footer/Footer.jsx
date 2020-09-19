c16d8a9cf01b30fc83c81a4a6c54078e29c04f90
Shira Feldinger <shirafeldinger@gmail.com>
2020-09-19 13:34:34 +0300

    added footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import "./FooterStyle.css";
 export default function Footer() {
   return (
     <div className="footer-div">
-      <p className="name">Shira Feldinger</p>
+      <p className="my-name">Shira Feldinger</p>
     </div>
   );
 }
