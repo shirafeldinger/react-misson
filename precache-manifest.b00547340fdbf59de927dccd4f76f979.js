a7bd4db61c44c241bf3d44c50acac99e7e3ff56f
Shira Feldinger <shirafeldinger@gmail.com>
2020-09-19 13:06:22 +0300

    Updates

--- precache-manifest.b00547340fdbf59de927dccd4f76f979.js
+++ precache-manifest.b00547340fdbf59de927dccd4f76f979.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fb600e846234908dcdccd89ad87c0ab3",
+    "revision": "2823f9ec19efe096abf6a056b8cab7ea",
     "url": "/react-misson/index.html"
   },
   {
-    "revision": "469c12891c19a83eefd6",
-    "url": "/react-misson/static/css/main.a7887a89.chunk.css"
+    "revision": "1b627b31cc4423d27fcf",
+    "url": "/react-misson/static/css/main.1adda78c.chunk.css"
   },
   {
     "revision": "822d8590247473b38666",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-misson/static/js/2.bf9c42a0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "469c12891c19a83eefd6",
+    "revision": "1b627b31cc4423d27fcf",
     "url": "/react-misson/static/js/main.31078cb2.chunk.js"
   },
   {
